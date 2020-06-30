bb6e181c3a3c13cdd65354f85d722ae3dfa5c954
bambooka <onceup0nAtime>
2020-06-30 15:00:29 +0300

    feat: add basic component structure

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="https://png.pngtree.com/element_our/png/20180912/coffee-time-png_91570.jpg" alt="logo"/>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main>
+      <img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" />
+      <div>
+        <img src="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg"/>
+        <p>dfgd</p>
+      </div>
+      <div>
+        my posts
+        <div>
+          new posts
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Main;
