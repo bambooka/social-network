8217b38f89c2206f53d396d28b2fa888665ebd37
bambooka <onceup0nAtime>
2020-06-30 16:23:17 +0300

    feat: add alt to images

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,9 +4,9 @@ import style from './../styles/Profile.module.css'
 const Profile = () => {
   return (
     <main className={style.profile}>
-      <img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" />
+      <img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" alt='profile wrapper' />
       <div>
-        <img src="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg"/>
+        <img src="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg" alt="avatar"/>
         <p>dfgd</p>
       </div>
       <div>
