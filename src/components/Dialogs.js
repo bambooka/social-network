657aa219b627909d876ca7b1c88fb3e7a5ae1e2b
bambooka <sumkina.ol@gmail.com>
2020-07-02 12:57:47 +0300

    feat: add navigation for dialogs

--- src/components/Dialogs.js
+++ src/components/Dialogs.js
@@ -1,10 +1,26 @@
 import React from "react";
 import style from './../styles/Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
   return(
     <div className={style.dialogs_content}>
-      dialogs
+      <div className={style.dialogs}>
+        <div className={style.dialogItem  + ' ' + style.active}>
+          <NavLink to={'/dialogs/1'}>Mila</NavLink>
+        </div>
+        <div className={style.dialogItem}>
+          <NavLink to={'/dialogs/2'}>Oleg</NavLink>
+        </div>
+        <div className={style.dialogItem}>
+          <NavLink to={'/dialogs/3'}>Eva</NavLink>
+        </div>
+      </div>
+      <div className={style.messages}>
+        <div>You are cool</div>
+        <div>You are sweet</div>
+        <div>You are angry</div>
+      </div>
     </div>
   )
 }
