9d5c93cc09bbe0fae1a99a3974a2af27e800305e
bambooka <sumkina.ol@gmail.com>
2020-07-08 14:44:48 +0300

    feat: move all data to separate file as object

---
+++ src/data/state.js
@@ -0,0 +1,34 @@
+let data = {
+    dialogs: [
+        {id: 1, author: 'Eva'},
+        {id: 2, author: 'Oleg'},
+        {id: 3, author: 'Ola'},
+        {id: 4, author: 'Max'}
+    ],
+    messages: [
+        {message: 'Hi'},
+        {message: 'Bye'},
+        {message: 'Good luck'},
+        {message: 'So so'},
+    ],
+    posts:  [
+        {
+            id: 1,
+            img: 'https://us.123rf.com/450wm/irinalevitskaya/irinalevitskaya1802/irinalevitskaya180200102/96364553-wow-pop-art-face-sexy-surprised-woman-with-blonde-curly-hair-and-open-mouth-holding-binoculars-in-he.jpg?ver=6',
+            text: 'It is my first post'
+        },
+        {
+            id: 2,
+            img: 'https://thumbs.dreamstime.com/b/print-146697723.jpg',
+            text: 'It is my second post'
+        },
+        {
+            id: 3,
+            img: 'https://image.freepik.com/free-vector/_1441-271.jpg',
+            text: 'It is my third post'
+        },
+    ]
+
+};
+
+export default data;
--- src/index.js
+++ src/index.js
@@ -3,42 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-const dialogs = [
-    {id: 1, author: 'Eva'},
-    {id: 2, author: 'Oleg'},
-    {id: 3, author: 'Ola'},
-    {id: 4, author: 'Max'}
-];
-
-const messages = [
-    {message: 'Hi'},
-    {message: 'Bye'},
-    {message: 'Good luck'},
-    {message: 'So so'},
-];
-
-const posts = [
-    {
-        id: 1,
-        img: 'https://us.123rf.com/450wm/irinalevitskaya/irinalevitskaya1802/irinalevitskaya180200102/96364553-wow-pop-art-face-sexy-surprised-woman-with-blonde-curly-hair-and-open-mouth-holding-binoculars-in-he.jpg?ver=6',
-        text: 'It is my first post'
-    },
-    {
-        id: 2,
-        img: 'https://thumbs.dreamstime.com/b/print-146697723.jpg',
-        text: 'It is my second post'
-    },
-    {
-        id: 3,
-        img: 'https://image.freepik.com/free-vector/_1441-271.jpg',
-        text: 'It is my third post'
-    },
-];
-
+import data from './data/state'
 ReactDOM.render(
   <React.StrictMode>
-    <App messages={messages} dialogs={dialogs} posts={posts}/>
+    <App messages={data.messages} dialogs={data.dialogs} posts={data.posts}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
