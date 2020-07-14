740fadc21e41749eb01df768f82e82caae64d3c9
bambooka <sumkina.ol@gmail.com>
2020-07-14 15:52:46 +0300

    feat: add message form markup

---
+++ src/components/Messages/MessageForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+let MessageForm = () => {
+  return (
+    <div>
+      <textarea value='this is textarea for message'/> <br />
+      <button>send</button>
+    </div>
+  )
+}
+
+export default MessageForm;
